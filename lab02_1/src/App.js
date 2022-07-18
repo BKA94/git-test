14f81ef4f0ffc7f185b7f2c77ece7213e8e73a52
Nguyen Nam <nam22041994@gmail.com>
2022-07-18 15:34:29 +0700

    update navbar

--- lab02_1/src/App.js
+++ lab02_1/src/App.js
@@ -1,17 +1,28 @@
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import { Navbar, NavbarBrand } from "reactstrap";
+import { STAFFS } from "./shared/staffs";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorance Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      staffs: STAFFS,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default App;
